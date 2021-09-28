579d847e1fd65e3ab4b9be5937bdccc121a84003
Wingemo <philip.wingemo@outlook.com>
2021-09-29 00:56:13 +0200

    Update bash.py

--- bash.py
+++ bash.py
@@ -3,16 +3,13 @@ import os
 
 bash()
 
-
 def ask(script):
     os.system("clear")
     print(script)
     return input()
 
-
 def bash():
     data = ask("BASH \n")
-
     if data == "pdf":
         pdf.execute(ask("PDF \n"))
 
