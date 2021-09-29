67a8f88880dec50bf690c19632cb1d53cf73e1c6
Wingemo <philip.wingemo@outlook.com>
2021-09-29 12:03:57 +0200

    Update bash.py

--- bash.py
+++ bash.py
@@ -1,35 +1,33 @@
 """
-This is a comment written in 
+This is a comment written in
 more than just one line
 """
 
-import pdf
-import loc
 import os
 import time
 
-if __name__ == "__main__":
-    bash()
-
 def ask(script):
-    os.system("clear")
+    os.system("cls")
     print(script)
     return input()
 
-def art(): 
-    art = with open('art.txt', 'r')
+def art():
+    art = open('art.txt', 'r')
     return art.read()
+
 
 def bash():
     data = ask(art())
     if data == "pdf":
-        pdf.execute(ask("PDF \n"))
+        ask("PDF \n")
         bash()
-    elif data == "loc"
-        loc.execute(ask("LOC \n"))
+    elif data == "loc":
+        ask("LOC \n")
         bash()
     else:
         print("ERROR")
         time.sleep(5)
         bash()
- 
+
+if __name__ == "__main__":
+    bash()
