e5fa5171dc974c9cc02dbf08667f7c2a1102e240
Wingemo <philip.wingemo@outlook.com>
2021-09-29 12:24:21 +0200

    Update bash.py

--- bash.py
+++ bash.py
@@ -5,28 +5,25 @@ more than just one line
 
 import os
 import time
+import loc
 
 def ask(script):
     os.system("cls")
     print(script)
     return input()
 
-def art():
-    art = open('art.txt', 'r')
-    return art.read()
-
 
 def bash():
-    data = ask(art())
+    data = ask("BASH")
     if data == "pdf":
         ask("PDF \n")
         bash()
     elif data == "loc":
-        ask("LOC \n")
+        loc.execute(ask("LOC \n"))
         bash()
     else:
         print("ERROR")
-        time.sleep(5)
+        time.sleep(2)
         bash()
 
 if __name__ == "__main__":
