23f20b2efced61bd69c7b200a8a582d0ae90f6b3
Wingemo <philip.wingemo@outlook.com>
2021-09-29 02:04:05 +0200

    Update pdf.py

--- pdf.py
+++ pdf.py
@@ -8,7 +8,7 @@ import os
 result = []
 
 def execute(filename):
-    for root, dir, files in os.walk(search_path):
+    for root, dir, files in os.walk("C:/"):
         if sys.argv in files:
             os.startfile(filename)
             result.append(os.path.join(root, filename))
